b12d31454519b0ca73ac2705ff94897d98ab755d
DaniAkiode <dannyakiode@hotmail.co.uk>
2024-03-05 11:47:43 +0000

    cancel jobs

--- Scheduler/Scheduler.py
+++ Scheduler/Scheduler.py
@@ -9,8 +9,14 @@ def job():
 #schedule.every(5).seconds.do(job)
 #schedule.every().day.at("11:38").do(job)
 
-schedule.every().minute.at(":40").do(job)
+j = schedule.every(1).to(5).seconds.do(job)
+
+counter = 0 
 
 while True:
     schedule.run_pending()
     tm.sleep(1)
+    counter += 1 
+    if counter == 10:
+        schedule.cancel_job(j)
+
