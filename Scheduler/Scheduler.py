e837f44da284ed1c64cce6afb2573c2cb6950108
DaniAkiode <dannyakiode@hotmail.co.uk>
2024-03-05 12:00:29 +0000

    Take a break! You are melting

--- Scheduler/Scheduler.py
+++ Scheduler/Scheduler.py
@@ -3,16 +3,12 @@ from schedule import every, repeat
 import time as tm
 from datetime import time, timedelta, datetime
 
-@repeat(every(5).seconds, message="Subscibe")
-@repeat(every(2).seconds, message="Hey")
-def job(message):
-    print("Hello the message is:", message)
+def break_reminder():
+    print("Take a break! You have been working for 30 minutes")
 
-schedule.every().seconds.do(job, message="HELLO")
 
+schedule.every().day.at("10:00").do(break_reminder)
 
 while True:
     schedule.run_pending()
     tm.sleep(1)
-
-
