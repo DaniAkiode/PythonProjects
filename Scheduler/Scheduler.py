9ec0f464eff7394523467f5127204b1cee87d3ac
DaniAkiode <dannyakiode@hotmail.co.uk>
2024-03-05 11:55:05 +0000

    HELLO HEY HI SUBSCIBE

--- Scheduler/Scheduler.py
+++ Scheduler/Scheduler.py
@@ -1,22 +1,18 @@
 import schedule 
+from schedule import every, repeat
 import time as tm
 from datetime import time, timedelta, datetime
 
+@repeat(every(5).seconds, message="Subscibe")
+@repeat(every(2).seconds, message="Hey")
+def job(message):
+    print("Hello the message is:", message)
 
-def job():
-    print("Get me outta here XD")
+schedule.every().seconds.do(job, message="HELLO")
 
-#schedule.every(5).seconds.do(job)
-#schedule.every().day.at("11:38").do(job)
-
-j = schedule.every(1).to(5).seconds.do(job)
-
-counter = 0 
 
 while True:
     schedule.run_pending()
     tm.sleep(1)
-    counter += 1 
-    if counter == 10:
-        schedule.cancel_job(j)
+
 
