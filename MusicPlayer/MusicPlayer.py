a8c8803c71de7f35a803c7c3b3fe8f85f20e9e20
DaniAkiode <dannyakiode@hotmail.co.uk>
2024-03-12 16:05:58 +0000

    Changes

--- MusicPlayer/MusicPlayer.py
+++ MusicPlayer/MusicPlayer.py
@@ -9,6 +9,7 @@ root.geometry("500x300")
 
 pygame.mixer.init() #initiate mixer in python in order to play autio
 
+
 songlist = Listbox(root, bg="black", fg="white", width=100, height=15 )
 songlist.pack() #add on window 
 
@@ -31,8 +32,6 @@ play_btn.grid(row=0, column=1, padx=7, pady=10)
 pause_btn.grid(row=0, column=2, padx=7, pady=10)
 next_btn.grid(row=0, column=3, padx=7, pady=10)
 prev_btn.grid(row=0, column=0, padx=7, pady=10)
-
-
 
 root.mainloop()
 
