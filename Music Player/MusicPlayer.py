71d8728c139553765b043dd42eb81932d45083fe
DaniAkiode <dannyakiode@hotmail.co.uk>
2024-03-12 13:21:39 +0000

    Initial interface

---
+++ Music Player/MusicPlayer.py
@@ -0,0 +1,18 @@
+from tkinter import * 
+import pygame 
+import os
+
+
+root = Tk()
+root.title("Music Player")
+root.geometry("500x300")
+
+pygame.mixer.init() #initiate mixer in python in order to play autio
+
+root.mainloop()
+
+
+
+
+
+
