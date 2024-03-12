b39740c535a33a91217966cf995a3ca702be02bc
DaniAkiode <dannyakiode@hotmail.co.uk>
2024-03-12 13:27:45 +0000

    Black Box

--- Music Player/MusicPlayer.py
+++ Music Player/MusicPlayer.py
@@ -9,6 +9,12 @@ root.geometry("500x300")
 
 pygame.mixer.init() #initiate mixer in python in order to play autio
 
+songlist = Listbox(root, bg="black", fg="white", width=100, height=15 )
+songlist.pack() #add on window 
+
+
+
+
 root.mainloop()
 
 
