58acdf7d27d55dfaa02b6378fe070c3e544e44e6
DaniAkiode <dannyakiode@hotmail.co.uk>
2024-03-12 13:49:52 +0000

    buttons now displayed

--- MusicPlayer/MusicPlayer.py
+++ MusicPlayer/MusicPlayer.py
@@ -18,10 +18,21 @@ next_btn_image = PhotoImage(file="C:\\Users\\danny\\PythonProjects\\MusicPlayer\
 prev_btn_image = PhotoImage(file="C:\\Users\\danny\\PythonProjects\\MusicPlayer\\previous.png")
 
 control_frame = Frame(root)
-control_frame.pack()
+control_frame.pack() #"displays" frame
 
 
-#play_btn = Button(control_frame, image=play_btn_image)
+play_btn = Button(control_frame, image=play_btn_image, borderwidth=0)
+pause_btn = Button(control_frame, image=pause_btn_image, borderwidth=0)
+next_btn = Button(control_frame, image=next_btn_image, borderwidth=0)
+prev_btn = Button(control_frame, image=prev_btn_image, borderwidth=0)
+
+
+play_btn.grid(row=0, column=1, padx=7, pady=10)
+pause_btn.grid(row=0, column=2, padx=7, pady=10)
+next_btn.grid(row=0, column=3, padx=7, pady=10)
+prev_btn.grid(row=0, column=0, padx=7, pady=10)
+
+
 
 root.mainloop()
 
