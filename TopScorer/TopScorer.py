4b0cf72a12d2480686ac87dab9d58ab4366e2d7f
DaniAkiode <dannyakiode@hotmail.co.uk>
2024-04-01 13:33:26 +0100

    Sort withouth the sorted function

--- TopScorer/TopScorer.py
+++ TopScorer/TopScorer.py
@@ -1,13 +1,23 @@
 # Dictionary containing players and the goals they have scored prior to April 2024
 players = {"Cristiano Ronaldo": 754, "Lionel Messi": 731, "Erling Haaland": 236, "Kylian Mbappe": 281}
 
-# Custom function to get the score of a player
-def get_goals(player):
-    return player[1]
+# Convert dictionary to a list of tuples
+players_list = list(players.items())
 
-# Sorting the players based on their scores, using the custom function as key
-sorted_players = sorted(players.items(), key=get_goals, reverse=True)
+#[('Cristiano Ronaldo', 754), ('Lionel Messi', 731), ('Erling Haaland', 236), ('Kylian Mbappe', 281)]
 
-# Printing the sorted players
-for player, goals in sorted_players:
+# Selection sort algorithm
+for i in range(len(players_list)):
+    min_idx = i  # 0 1 2 3 
+    for j in range(i+1, len(players_list)):
+        if players_list[j][1] > players_list[min_idx][1]:
+            min_idx = j
+
+    # Swap the minimum element with the current element
+    players_list[i], players_list[min_idx] = players_list[min_idx], players_list[i]
+
+# Print the sorted list
+for player, goals in players_list:
     print(player, goals)
+    
+    
