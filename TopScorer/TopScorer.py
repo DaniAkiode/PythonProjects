313c61303b7c5d14555a34b59c07b7e26c92a793
DaniAkiode <dannyakiode@hotmail.co.uk>
2024-04-01 11:45:44 +0100

    Sorted

--- TopScorer/TopScorer.py
+++ TopScorer/TopScorer.py
@@ -1,4 +1,10 @@
+# Dictionary containing players and the goals they have scored prior to April 2024
+players = {"Cristiano Ronaldo": 754, "Lionel Messi": 731, "Erling Haaland": 236, "Kylian Mbappe": 281}
 
-ballerz = {"Cristiano Ronaldo": 754, "Lionel Messi": 731, "Erling Haaland": 236, "Killina Mbappe": 281,}
+def get_goals(player):
+    return player[1]
 
-print(ballerz['Cristiano Ronaldo'])
+sorted_players = sorted(players.items(), key=get_goals, reverse=True)
+
+for player, goals in sorted_players:
+    print(player, goals)
